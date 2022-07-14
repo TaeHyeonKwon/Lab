a7e3fddcb30f469b31a163d0717e66deb403d197
TaeHyeonKwon <74909565+TaeHyeonKwon@users.noreply.github.com>
2022-07-14 11:31:55 +0900

    Create DDPG.py

---
+++ RL_공부방/DDPG.py
@@ -0,0 +1,120 @@
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Actor network
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 128)
+        self.l2 = nn.Linear(128, 64)
+        self.l3 = nn.Linear(64, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+
+# Critic network
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 128)
+        self.l2 = nn.Linear(128 + action_dim, 64)
+        self.l3 = nn.Linear(64, 1)
+
+    def forward(self, state, action):
+        q = F.relu(self.l1(state))
+        q = F.relu(self.l2(torch.cat([q, action], 1)))
+        return self.l3(q)
+
+
+class DDPG(object):
+    def __init__(self, state_dim, action_dim, max_action):
+
+        # hyper parameter
+        self.learning_rate = 1e-4
+        self.gamma = 0.99
+        self.tau = 0.001
+        self.weight_decay = 1e-2
+
+        # Network load
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = copy.deepcopy(self.actor)        # Actor 네트워크 복제
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.learning_rate)  # optimizer initialize
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = copy.deepcopy(self.critic)     # Critic 네트워크 복제
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), weight_decay=self.weight_decay)  # optimizer initialize
+
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)  # state tensor 변환
+        return self.actor(state).cpu().data.numpy().flatten()  # actor 네트워크로부터 액션을 뽑아내고 numpy로 변환
+
+    def train(self, replay_buffer):
+        # batch_size parameter
+        self.batch_size = 64
+
+        # Sample replay buffer
+        state, action, next_state, reward, not_done = replay_buffer.sample(self.batch_size)
+
+        # Compute the target Q value
+        target_Q = self.critic_target(next_state, self.actor_target(next_state))
+        target_Q = reward + (not_done * self.gamma * target_Q).detach()
+
+        # Get current Q estimate
+        current_Q = self.critic(state, action)
+
+        # Compute critic loss
+        critic_loss = F.mse_loss(current_Q, target_Q)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Compute actor loss
+        actor_loss = -self.critic(state, self.actor(state)).mean()
+
+        # Optimize the actor
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Update the frozen target models (soft update)
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+
+
+    # Model save
+    def save(self, filename):
+        torch.save(self.critic.state_dict(), filename + "_critic")
+        torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
+
+        torch.save(self.actor.state_dict(), filename + "_actor")
+        torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
+
+
+    # Model load
+    def load(self, filename):
+        self.critic.load_state_dict(torch.load(filename + "_critic"))
+        self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
+        self.critic_target = copy.deepcopy(self.critic)
+
+        self.actor.load_state_dict(torch.load(filename + "_actor"))
+        self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
+        self.actor_target = copy.deepcopy(self.actor)
